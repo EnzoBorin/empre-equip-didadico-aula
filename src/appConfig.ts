6cd12cb0353b83f92bc1c35f54a31981a977166a
Enzo Cardoso Borin <enzooborin@gmail.com>
2025-08-27 09:29:05 -0300

    Mudanças

--- src/appConfig.ts
+++ src/appConfig.ts
@@ -1,10 +1,23 @@
 const SERVER_ROUTES = {
+        LISTAR_USUARIOS: '/lista/usuario',
+        NOVO_USUARIO: '/novo/usuario',
+        REMOVER_USUARIO: '/remove/usuario',
+        ATUALIZAR_USUARIO: '/atualiza/usuario',
+        UNICO_USUARIO: '/lista/usuarioUnico',
 
-    NOVO_USUARIO: '/novo/usuario',
-    ATUALIZAR_USUARIO: '/atualiza/usuario',
-    REMOVER_USUARIO: '/remove/usuario',
-    UNICO_USUARIO: '/lista/usuarioUnico',
-    LISTAR_USUARIO: '/lista/usuario'   
-     
-}
-export { SERVER_ROUTES }
+        LISTAR_EQUIPAMENTOS: '/lista/equipamento',
+        NOVO_EQUIPAMENTO: '/novo/equipamento',
+        REMOVER_EQUIPAMENTO: '/remove/equipamento',
+        ATUALIZAR_EQUIPAMENTO: '/atualiza/equipamento',
+
+        LISTAR_EMPRESTIMOS: '/lista/emprestimo',
+        NOVO_EMPRESTIMO: '/novo/emprestimo',
+        REMOVER_EMPRESTIMO: '/remove/emprestimo',
+        ATUALIZAR_EMPRESTIMO: '/atualiza/emprestimo',
+        LISTAR_EMPRESTIMO_DETALHADOS: '/lista/emprestimoDetalhado',
+        
+
+    
+    }
+    
+    export { SERVER_ROUTES }
